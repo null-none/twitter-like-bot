7f41d9a1908a53a755492a04895e97dca7145f57
Martin Malinowski <martinmalinowski0@gmail.com>
2017-07-31 04:36:07 +0100

    Add like event function completed

--- twitter_like_bot.js
+++ twitter_like_bot.js
@@ -1,20 +1,28 @@
 var twitter = require('twitter');
+var config = require('./authentication.js');
 var events = require('events');
 
-var connect = new twitter(authentication)
+
+var connect = new twitter(config)
 var eventEmitter = new events.EventEmitter();
 
-var keywords = {track:'keyword'};
+var keywords = {track:'keyword1','keyword2'};
 var stream = connect.stream('statuses/filter', keywords);
 
-function likeTweet = function(tweetId) {
-
+var likeTweet = function(tweetId) {
+  connect.post('favorites/create', tweetId, function(err, response) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Liked! - tweet id: '+tweetId);
+    }
+  })
 }
 
-eventEmitter.listen('like', likeTweet);
+eventEmitter.on('like', likeTweet);
 
 stream.on('data', function(data) {
-  eventEmitter.emit('like', data.id_str);
+  eventEmitter.emit('like', {data:data.id_str});
 })
 stream.on('error', function(err) {
   console.log(err);
